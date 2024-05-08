b92ccb6876184db2f4ac81e36d5e802526a77d69
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-05-08 14:32:51 +0200

    Update nn_inference.py

--- nn_inference.py
+++ nn_inference.py
@@ -607,41 +607,42 @@ for test_img in os.listdir(test_img_path):
 
     count = count+1
 
-    # Convert image to grayscale
-    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    # # Convert image to grayscale
+    # gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
     
-    # Use canny edge detection
-    edges = cv2.Canny(gray,50,150,apertureSize=3)
+    # # Use canny edge detection
+    # edges = cv2.Canny(gray,50,150,apertureSize=3)
     
-    reader = easyocr.Reader(['en'])
-    result = reader.readtext(gray, detail = 0)
-    pxum_r = result[0]
-    psum = re.sub("[^0-9]", "", pxum_r)
-    # print(psum)
+    # reader = easyocr.Reader(['en'])
+    # result = reader.readtext(gray, detail = 0)
+    # pxum_r = result[0]
+    # psum = re.sub("[^0-9]", "", pxum_r)
+    # # print(psum)
     
-    # Apply HoughLinesP method to
-    # to directly obtain line end points
-    lines_list =[]
-    lines = cv2.HoughLinesP(
-                edges, # Input edge image
-                1, # Distance resolution in pixels
-                np.pi/180, # Angle resolution in radians
-                threshold=100, # Min number of votes for valid line
-                minLineLength=100, # Min allowed length of line
-                maxLineGap=1 # Max allowed gap between line for joining them
-                )
+    # # Apply HoughLinesP method to
+    # # to directly obtain line end points
+    # lines_list =[]
+    # lines = cv2.HoughLinesP(
+    #             edges, # Input edge image
+    #             1, # Distance resolution in pixels
+    #             np.pi/180, # Angle resolution in radians
+    #             threshold=100, # Min number of votes for valid line
+    #             minLineLength=100, # Min allowed length of line
+    #             maxLineGap=1 # Max allowed gap between line for joining them
+    #             )
     
-    # Iterate over points
-    for points in lines:
-        # Extracted points nested in the list
-        x1,y1,x2,y2=points[0]
-        # Draw the lines joing the points
-        # On the original image
-        cv2.line(im,(x1,y1),(x2,y2),(0,255,0),2)
-        # Maintain a simples lookup list for points
-        lines_list.append([(x1,y1),(x2,y2)])
-        scale_len = sqrt((x2-x1)**2+(y2-y1)**2)
-        um_pix = float(psum)/scale_len    
+    # # Iterate over points
+    # for points in lines:
+    #     # Extracted points nested in the list
+    #     x1,y1,x2,y2=points[0]
+    #     # Draw the lines joing the points
+    #     # On the original image
+    #     cv2.line(im,(x1,y1),(x2,y2),(0,255,0),2)
+    #     # Maintain a simples lookup list for points
+    #     lines_list.append([(x1,y1),(x2,y2)])
+    #     scale_len = sqrt((x2-x1)**2+(y2-y1)**2)
+    #     um_pix = float(psum)/scale_len    
+    um_pix = 1
 
     GetInference()
     GetCounts()
