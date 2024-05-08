9923142a92d0f54b9267f55e2da0d6c6183ad5a7
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-05-08 12:35:40 +0200

    Update scale_measure.py

--- scale_measure.py
+++ scale_measure.py
@@ -1,19 +1,55 @@
+from PIL import Image
 import cv2
+import numpy as np
+from numpy import sqrt
+from google.colab.patches import cv2_imshow
+import easyocr
+import re
+
 test_img_path = "/home/deamoon_uw_nn/DATASET/INFERENCE/"
 # Load image, convert to grayscale, Otsu's threshold
 for test_img in os.listdir(test_img_path):
-    image = cv2.imread(test_img)
-    result = image.copy()
-    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+    # Read image
+    image = cv2.imread('/content/70.tif')
     
-    # Detect horizontal lines
-    horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (40,1))
-    detect_horizontal = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
-    cnts = cv2.findContours(detect_horizontal, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    for c in cnts:
-        cv2.drawContours(result, [c], -1, (36,255,12), 2)
-       
-    cv2.imwrite('result_scale.png', result)
-    cv2.waitKey()
+    # Convert image to grayscale
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    
+    # Use canny edge detection
+    edges = cv2.Canny(gray,50,150,apertureSize=3)
+    
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(gray, detail = 0)
+    pxum_r = result[0]
+    psum = re.sub("[^0-9]", "", pxum_r)
+    print(psum)
+    
+    # Apply HoughLinesP method to
+    # to directly obtain line end points
+    lines_list =[]
+    lines = cv2.HoughLinesP(
+    			edges, # Input edge image
+    			1, # Distance resolution in pixels
+    			np.pi/180, # Angle resolution in radians
+    			threshold=100, # Min number of votes for valid line
+    			minLineLength=100, # Min allowed length of line
+    			maxLineGap=1 # Max allowed gap between line for joining them
+    			)
+    
+    # Iterate over points
+    for points in lines:
+        # Extracted points nested in the list
+        x1,y1,x2,y2=points[0]
+        # Draw the lines joing the points
+        # On the original image
+        cv2.line(image,(x1,y1),(x2,y2),(0,255,0),2)
+        # Maintain a simples lookup list for points
+        lines_list.append([(x1,y1),(x2,y2)])
+        scale_len = sqrt((x2-x1)**2+(y2-y1)**2)
+        um_pix = float(psum)/scale_len
+    
+    print (um_pix)
+    
+    # Save the result image
+    cv2.imwrite('detectedLines.png',image)
+    cv2_imshow(image)
