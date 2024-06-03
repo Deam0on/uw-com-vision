fcf2b139e73f675d67e0077b0f535add1670138e
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-06-03 11:01:38 +0200

    Update nn_inference.py

--- nn_inference.py
+++ nn_inference.py
@@ -463,7 +463,7 @@ for x_pred in [0,1]:
     
             lines_list =[]
             lines = cv2.HoughLinesP(
-                        edges, # Input edge image
+                        im, # Input edge image
                         1, # Distance resolution in pixels
                         np.pi/180, # Angle resolution in radians
                         threshold=100, # Min number of votes for valid line
