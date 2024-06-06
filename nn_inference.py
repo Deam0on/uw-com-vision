cd556254be01695bb6fe50ceae1721911f3362c9
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-06-06 14:16:38 +0200

    Update nn_inference.py

--- nn_inference.py
+++ nn_inference.py
@@ -1,33 +1,60 @@
 ## IMPORTS
 import os
+from os import listdir
 import time
+import glob
+import random
+import copy
 import json
+import datetime
+from datetime import timedelta
 import shutil
+from distutils import file_util, dir_util
+from distutils.dir_util import copy_tree
+from contextlib import redirect_stdout
+import tempfile
+import statistics
+import scipy
+from scipy.spatial import distance as dist
+import imutils
+from imutils import perspective
+from imutils import contours
 import csv
 import torch
+import torchvision
 import cv2
 import numpy as np
 import itertools
+from itertools import groupby
 import pandas as pd
-from datetime import datetime, timedelta
 from matplotlib import pyplot as plt
+from scipy.stats import norm
 from PIL import Image
+import seaborn as sns
+import shapely
 from shapely.geometry import Point
 from shapely.affinity import scale, rotate
+import matplotlib.pyplot as plt
 import detectron2.data.transforms as T
 from detectron2.structures import BoxMode
 from detectron2.data import detection_utils as utils
 from detectron2.engine import DefaultTrainer
 from detectron2.data import build_detection_test_loader, build_detection_train_loader
 from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.utils.visualizer import Visualizer
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
+from detectron2.data.datasets import register_coco_instances
+from detectron2.utils.visualizer import ColorMode
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+import pycocotools.mask as mask_util
+from skimage.measure import find_contours
 from skimage.measure import label
 from scipy.ndimage import binary_fill_holes
 from skimage.morphology import dilation, erosion
-import optuna
+from google.cloud import storage
+
 import easyocr
 import re
 from numpy import sqrt
@@ -105,8 +132,9 @@ def get_superannotate_dicts(img_dir, label_dir):
     return dataset_dicts
 
 ## Def custom mapper, rand changes to dataset imgs, induce variability to dataset
-def custom_mapper(dataset_dict):
-    image = utils.read_image(dataset_dict["file_name"], format="BGR")
+def custom_mapper(dataset_dicts):
+    dataset_dicts = copy.deepcopy(dataset_dicts)  # it will be modified by code below
+    image = utils.read_image(dataset_dicts["file_name"], format="BGR")
     transform_list = [
         T.Resize((800,800)),
         T.RandomBrightness(0.8, 1.8),
@@ -116,20 +144,19 @@ def custom_mapper(dataset_dict):
         T.RandomLighting(0.7),
         T.RandomFlip(prob=0.4, horizontal=False, vertical=True),
     ]
-
+  
     image, transforms = T.apply_transform_gens(transform_list, image)
-    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+    dataset_dicts["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
 
     annos = [
         utils.transform_instance_annotations(obj, transforms, image.shape[:2])
-        for obj in dataset_dict.pop("annotations")
+        for obj in dataset_dicts.pop("annotations")
         if obj.get("iscrowd", 0) == 0
     ]
   
     instances = utils.annotations_to_instances(annos, image.shape[:2])
-    dataset_dict["instances"] = utils.filter_empty_instances(instances)
-    return dataset_dict
-
+    dataset_dicts["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dicts
 
 ## Replace default trainer, subs 2x 1D method
 class CustomTrainer(DefaultTrainer):
@@ -150,19 +177,35 @@ multiclass_test_metadata = MetadataCatalog.get("multiclass_Test").set( thing_cla
 
 
 def get_image_folder_path(base_path='/home/deamoon_uw_nn/DATASET/INFERENCE'):
+    """
+    This function checks whether the images are in the base folder or in the UPLOAD subfolder.
+    It returns the path to the folder containing the images.
+
+    Parameters:
+    base_path (str): The base path where the INFERENCE folder is located.
+
+    Returns:
+    str: The path to the folder containing the images.
+    """
+    # Define the two possible paths
     inference_path = os.path.join(base_path)
     upload_path = os.path.join(base_path, 'UPLOAD')
 
-    if os.path.exists(inference_path) and any(os.path.isfile(os.path.join(inference_path, f)) for f in os.listdir(inference_path)):
+    # Check if the INFERENCE folder contains images
+    if any(os.path.isfile(os.path.join(inference_path, f)) for f in os.listdir(inference_path)):
         return inference_path
+
+    # Check if the UPLOAD subfolder contains images
     elif os.path.exists(upload_path) and any(os.path.isfile(os.path.join(upload_path, f)) for f in os.listdir(upload_path)):
         return upload_path
+
+    # If no images found in either folder, return None or raise an exception
     else:
         raise FileNotFoundError("No images found in INFERENCE or INFERENCE/UPLOAD folders.")
 
+# Example usage:
+# Assuming you are running the script from the same directory where the INFERENCE folder is located
 image_folder_path = get_image_folder_path()
-
-
 
 ## Collect prediction masks
 # Convert binary_mask to RLE
@@ -242,68 +285,84 @@ def rle_decode(mask_rle, shape):
         img[lo:hi] = 1
     return img.reshape(shape) # Needed to align to RLE direction ##
 
-def rle_encode(img):
-    pixels = img.flatten()
-    if not np.any(pixels):
-        return ''
-    pixels = np.concatenate([[0], pixels, [0]])
-    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
-    runs[1::2] -= runs[::2]
-    return ' '.join(str(x) for x in runs)
-
+def rle_encoding(x):
+    # .T sets Fortran order down-then-right
+    dots = np.where(x.T.flatten() == 1)[0]
+    run_lengths = []
+    prev = -2
+    for b in dots:
+        if (b > prev+1):
+            run_lengths.extend((b+1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return run_lengths
 
 def postprocess_masks(ori_mask, ori_score, image, min_crys_size=2):
+
+    """Clean overlaps between bounding boxes, """
+    """ fill small holes, smooth boundaries"""
+    image = image[:, :,::-1]
     height, width = image.shape[:2]
 
-    if len(ori_mask) == 0 or np.max(ori_score) < 0.5:
-        return []
+    score_threshold = 0.5
 
-    valid_masks = [ori_mask[i] for i in range(len(ori_mask)) if np.sum(ori_mask[i]) > min_crys_size]
-    if not valid_masks:
-        return []
+    if len(ori_mask) == 0 or ori_score.all() < score_threshold:
+        return
 
-    masks = []
+    keep_ind = np.where(np.sum(ori_mask, axis=(0, 1)) > min_crys_size)[0]
+    if len(keep_ind) < len(ori_mask):  # keep_ind possible to be zero zero
+        if(keep_ind.shape[0] != 0):
+            ori_mask = ori_mask[:keep_ind.shape[0]]  # shape[0]:to int
+            ori_score = ori_score[:keep_ind.shape[0]]
+        else:
+            ori_mask = []
+            ori_score = []
+
     overlap = np.zeros([height, width])
 
-    for mask in valid_masks:
-        mask = binary_fill_holes(mask).astype(np.uint8)
+    masks = []
+  
+    # Removes overlaps from masks with lower score
+    for i in range(len(ori_mask)):
+        # Fill holes inside the mask
+        mask = binary_fill_holes(ori_mask[i]).astype(np.uint8)
+        # Smoothen edges using dilation and erosion
         mask = erosion(dilation(mask))
+        # Delete overlaps
         overlap += mask
         mask[overlap > 1] = 0
-        if np.max(label(mask)) <= 1:
-            masks.append(mask)
+        out_label = label(mask)
+        # Remove all the pieces if there are more than one pieces
+        if out_label.max() > 1:
+            mask[()] = 0
+
+        masks.append(mask)
+
     return masks
 
-
-# path = "./output/"  # the weight save path
-# inpath = image_folder_path
-# images_name = listdir(inpath)
-# images_name = [f for f in os.listdir(inpath) if f.endswith('.tif')]
+path = "./output/"  # the weight save path
+inpath = image_folder_path
+images_name = listdir(inpath)
+images_name = [f for f in os.listdir(inpath) if f.endswith('.tif')]
 
 Img_ID = []
 EncodedPixels = []
 num = 0
 conv = lambda l: ' '.join(map(str, l))
 
-path = "./output/"
-inpath = image_folder_path
-images_name = [f for f in os.listdir(inpath) if f.endswith('.tif')]
-
 for name in images_name:
-    image = cv2.imread(os.path.join(inpath, name))
-    if image is None:
-        print(f"Error loading image {name}, skipping...")
-        continue
+    image = cv2.imread(inpath + "/" + name)
     outputs = predictor(image)
+    num += 1
+    # print(np.asarray(outputs["instances"].to('cpu')._fields['pred_masks'][0]).shape)
     masks = postprocess_masks(
         np.asarray(outputs["instances"].to('cpu')._fields['pred_masks']),
-        outputs["instances"].to('cpu')._fields['scores'].numpy(),
-        image
-    )
-    if masks:
-        for i, mask in enumerate(masks):
-            Img_ID.append(name.replace('.tif', ''))
-            EncodedPixels.append(rle_encode(mask))
+            outputs["instances"].to('cpu')._fields['scores'].numpy(), image)
+
+    if masks:  # If any objects are detected in this image
+            for i in range(len(masks)):  # Loop all instances
+                Img_ID.append(name.replace('.tif', ''))
+                EncodedPixels.append(conv(rle_encoding(masks[i])))
 
 ## save inference 
 df = pd.DataFrame({"ImageId": Img_ID, "EncodedPixels": EncodedPixels})
@@ -346,61 +405,144 @@ def GetCounts():
 ## get mask contours for outlines / ferret
 # def GetMask_Contours():
           
-for x_pred in [0, 1]:
+for x_pred in [0,1]:
+
+    ## create and append lists
+    # lengthList = list()
+    # widthList = list()
+    # circularEDList = list()
+    # aspectRatioList = list()
+    # circularityList = list()
+    # chordsList = list()
+    # ferretList = list()
+    # roundList = list()
+    # sphereList = list()
+    TList = list()
+    PList = list()
+    # psum_list = list()
+    # name_list = list()
+    tT = 0
+    tP = 0
+    count = 0    
     csv_filename = f'results_x_pred_{x_pred}.csv'
+    test_img_path = image_folder_path
+    x_th = len(test_img_path)
+    x_c = 0
+
+
+
+    # Open CSV file before processing images
     with open(csv_filename, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
+        # Write the header row (adjust this as per your data)
         csvwriter.writerow(['length', 'width', 'circularED', 'aspectRatio', 'circularity', 'chords', 'ferret', 'round', 'sphere', 'psum', 'name'])
     
         for test_img in os.listdir(test_img_path):
+    
+            # Write the measurements and descriptors to the CSV file
+            # classes_of_interest = [keywds.index(k)]
             input_path = os.path.join(test_img_path, test_img)
             im = cv2.imread(input_path)
-            if im is None:
-                print(f"Error loading image {test_img}, skipping...")
-                continue
             source_image_filename = test_img
-            gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-            edges = cv2.Canny(gray, 50, 150, apertureSize=3)
-            reader = easyocr.Reader(['en'])
-            result = reader.readtext(gray, detail=0, paragraph=False)
-            psum = re.sub("[^0-9]", "", result[0] if result else "1")
-            lines_list = []
-            lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=100, maxLineGap=1)
+        
+            count = count+1
+        
+            # Convert image to grayscale
+            gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
             
-            for points in lines:
-                x1, y1, x2, y2 = points[0]
-                cv2.line(im, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                scale_len = sqrt((x2 - x1)**2 + (y2 - y1)**2)
-                um_pix = float(psum) / scale_len
+            # Use canny edge detection
+            edges = cv2.Canny(gray,50,150,apertureSize=3)
+            
+            reader = easyocr.Reader(['en'])
+            result = reader.readtext(gray, 
+                         detail=0,             # Return only the text
+                         paragraph=False,       # Treat each line separately
+                         contrast_ths=0.85,      # Increase contrast threshold
+                         adjust_contrast=0.85,   # Adjust contrast
+                         add_margin=0.25,        # Add margin around text
+                         width_ths=0.25,         # Width threshold for text boxes
+                         decoder='beamsearch')  # Use beamsearch for decoding
+            pxum_r = result[0]
+            psum = re.sub("[^0-9]", "", pxum_r)
 
+            # reader = easyocr.Reader(['en'])
+            # result = reader.readtext(gray, detail = 0)
+            # pxum_r = result[0]
+            # psum = re.sub("[^0-9]", "", pxum_r)
+        
+            lines_list =[]
+            lines = cv2.HoughLinesP(
+                        edges, # Input edge image
+                        1, # Distance resolution in pixels
+                        np.pi/180, # Angle resolution in radians
+                        threshold=100, # Min number of votes for valid line
+                        minLineLength=100, # Min allowed length of line
+                        maxLineGap=1 # Max allowed gap between line for joining them
+                        )
+            
+            # Iterate over points
+            for points in lines:
+                # Extracted points nested in the list
+                x1,y1,x2,y2=points[0]
+                # Draw the lines joing the points
+                # On the original image
+                cv2.line(im,(x1,y1),(x2,y2),(0,255,0),2)
+                # Maintain a simples lookup list for points
+                lines_list.append([(x1,y1),(x2,y2)])
+                scale_len = sqrt((x2-x1)**2+(y2-y1)**2)
+                um_pix = float(psum)/scale_len    
+            # um_pix = 1
+        
             GetInference()
             GetCounts()
 
             outputs = predictor(im)
-            filtered_instances = outputs['instances'][outputs['instances'].pred_classes == x_pred]
+            
+            # Get all instances
+            inst_out = outputs['instances']
+            
+            # Filter instances where predicted class is 3
+            filtered_instances = inst_out[inst_out.pred_classes == x_pred]
+            
+            # Now extract the masks for these filtered instances
             mask_array = filtered_instances.pred_masks.to("cpu").numpy()
-
+            
+            # instances = instances[instances.pred_classes == 3]
+            # mask_array = outputs['instances'].pred_masks==3.to("cpu").numpy()
             num_instances = mask_array.shape[0]
             mask_array = np.moveaxis(mask_array, 0, -1)
+            mask_array_instance = []
             output = np.zeros_like(im)
+            fig = plt.figure(figsize=(15, 20))
             for i in range(num_instances):
-                output = np.where(mask_array[:, :, i] == True, 255, output)
+                mask_array_instance.append(mask_array[:, :, i:(i+1)])
+                output = np.where(mask_array_instance[i] == True, 255, output)
+            imm = Image.fromarray(output)
+            imm.save('predicted_masks.jpg')
+            cv2.imwrite('Masks.jpg', output) #mask
             im_mask = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
             cnts = cv2.findContours(im_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             cnts = imutils.grab_contours(cnts)
             
-            if cnts:
+            if len(cnts) > 0:
+              
                 (cnts, _) = contours.sort_contours(cnts)
-                pixelsPerMetric = None
+                pixelsPerMetric = 0.85
+                
                 for c in cnts:
                     if cv2.contourArea(c) < 100:
                         continue
                     area = cv2.contourArea(c)
                     perimeter = cv2.arcLength(c, True)
+                    
+                    orig = im_mask.copy()
                     box = cv2.minAreaRect(c)
                     box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
                     box = np.array(box, dtype="int")
                     box = perspective.order_points(box)
+                    cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+                    for (x, y) in box:
+                        cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
                     (tl, tr, br, bl) = box
                     (tltrX, tltrY) = midpoint(tl, tr)
                     (blbrX, blbrY) = midpoint(bl, br)
@@ -409,21 +551,26 @@ for x_pred in [0, 1]:
                     dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
                     dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
                     if pixelsPerMetric is None:
-                        pixelsPerMetric = dB / max(dA, dB)
+                        pixelsPerMetric = dB / width
                     dimA = dA / pixelsPerMetric
                     dimB = dB / pixelsPerMetric
-                    Aspect_Ratio = max(dimA, dimB) / min(dimA, dimB) if min(dimA, dimB) != 0 else 0
-                    Length = min(dimA, dimB) * um_pix
-                    Width = max(dimA, dimB) * um_pix
-                    CircularED = np.sqrt(4 * area / np.pi) * um_pix
-                    Chords = perimeter * um_pix
-                    Roundness = 1 / Aspect_Ratio if Aspect_Ratio != 0 else 0
-                    Sphericity = (2 * np.sqrt(np.pi * area / pixelsPerMetric)) / perimeter * um_pix
-                    Circularity = 4 * np.pi * (area / (perimeter ** 2)) * um_pix
-                    Feret_diam = max(dimA, dimB) * um_pix
-
+                    dimArea = area/pixelsPerMetric
+                    dimPerimeter = perimeter/pixelsPerMetric
+                    diaFeret = max(dimA, dimB)
+                    if (dimA and dimB) !=0:
+                        Aspect_Ratio = max(dimB,dimA)/min(dimA,dimB)
+                    else:
+                        Aspect_Ratio = 0
+                    Length = min(dimA, dimB)*um_pix
+                    Width = max(dimA, dimB)*um_pix
+                    CircularED = np.sqrt(4*area/np.pi)*um_pix
+                    Chords = cv2.arcLength(c,True)*um_pix
+                    Roundness = 1/(Aspect_Ratio) if Aspect_Ratio != 0 else 0
+                    Sphericity = (2*np.sqrt(np.pi*dimArea))/dimPerimeter*um_pix
+                    Circularity = 4*np.pi*(dimArea/(dimPerimeter)**2)*um_pix
+                    Feret_diam = diaFeret*um_pix
+    
                     csvwriter.writerow([Length, Width, CircularED, Aspect_Ratio, Circularity, Chords, Feret_diam, Roundness, Sphericity, psum, test_img])
-
     
             # Create a CSV file at the end of each iteration of the outer loop
             # csv_filename = f'results_x_pred_{x_pred}.csv'
