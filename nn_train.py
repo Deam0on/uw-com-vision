558254fd66a64e0a6ff7e17fa1fb7342a04a5c5d
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-06-06 13:55:20 +0200

    Update nn_train.py

--- nn_train.py
+++ nn_train.py
@@ -1,59 +1,33 @@
 ## IMPORTS
 import os
-from os import listdir
 import time
-import glob
-import random
-import copy
 import json
-import datetime
-import fiftyone as fo
-from datetime import timedelta
 import shutil
-from distutils import file_util, dir_util
-from distutils.dir_util import copy_tree
-from contextlib import redirect_stdout
-import tempfile
-import statistics
-from scipy.spatial import distance as dist
-import imutils
-from imutils import perspective
-from imutils import contours
 import csv
 import torch
-import torchvision
 import cv2
 import numpy as np
 import itertools
-from itertools import groupby
 import pandas as pd
+from datetime import datetime, timedelta
 from matplotlib import pyplot as plt
-from scipy.stats import norm
 from PIL import Image
-import seaborn as sns
-import shapely
 from shapely.geometry import Point
 from shapely.affinity import scale, rotate
-import matplotlib.pyplot as plt
 import detectron2.data.transforms as T
 from detectron2.structures import BoxMode
 from detectron2.data import detection_utils as utils
 from detectron2.engine import DefaultTrainer
 from detectron2.data import build_detection_test_loader, build_detection_train_loader
 from detectron2.data import DatasetCatalog, MetadataCatalog
-from detectron2.utils.visualizer import Visualizer
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultPredictor
-from detectron2.data.datasets import register_coco_instances
-from detectron2.utils.visualizer import ColorMode
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
-import pycocotools.mask as mask_util
-from skimage.measure import find_contours
 from skimage.measure import label
 from scipy.ndimage import binary_fill_holes
 from skimage.morphology import dilation, erosion
-from google.cloud import storage
+import optuna
 
 ## Def for dataset build, SA annotated data, SA format, WARNING, NO POLYLINES
 
@@ -110,24 +84,7 @@ def get_superannotate_dicts(img_dir, label_dir):
 
                         poly = [(x + 0.5, y + 0.5) for x, y in zip(px,py) ]
                         poly = [p for x in poly for p in x]
-                        
-                    # elif type == "polyline":
-                        
-                    #     height = imgs_anns["metadata"]["height"]
-                    #     width = imgs_anns["metadata"]["width"]
-
-                    #     px = anno["points"][0:-1:2]  #0 -1 2
-                    #     py = anno["points"][1:-1:2] # 1 -1 2
-                    #     px.append(anno["points"][0])    # 0
-                    #     py.append(anno["points"][-1])   # -1
-
-                    #     # poly = [(x + 0.5, y + 0.5) for x, y in zip(px,py) ]
-                    #     # poly = [p for x in poly for p in x]
-                        
-                    #     poly = [(x*width, y*height) for x, y in zip(px,py) ]
-                    #     poly = [p for x in poly for p in x]
-                      
-
+                    
                     if "throat" in categoryName :
                         category_id = 0
                     elif "pore" in categoryName :
@@ -146,46 +103,9 @@ def get_superannotate_dicts(img_dir, label_dir):
                 dataset_dicts.append(record)
     return dataset_dicts
 
-### FF1 test
-
-def get_fiftyone_dicts(samples):
-    samples.compute_metadata()
-
-    dataset_dicts = []
-    for sample in samples.select_fields(["id", "filepath", "metadata", "segmentations"]):
-        height = sample.metadata["height"]
-        width = sample.metadata["width"]
-        record = {}
-        record["file_name"] = sample.filepath
-        record["image_id"] = sample.id
-        record["height"] = height
-        record["width"] = width
-
-        objs = []
-        for det in sample.segmentations.detections:
-            tlx, tly, w, h = det.bounding_box
-            bbox = [int(tlx*width), int(tly*height), int(w*width), int(h*height)]
-            fo_poly = det.to_polyline()
-            poly = [(x*width, y*height) for x, y in fo_poly.points[0]]
-            poly = [p for x in poly for p in x]
-            obj = {
-                "bbox": bbox,
-                "bbox_mode": BoxMode.XYWH_ABS,
-                "segmentation": [poly],
-                "category_id": 0,
-            }
-            objs.append(obj)
-
-        record["annotations"] = objs
-        dataset_dicts.append(record)
-
-    return dataset_dicts
-
-
 ## Def custom mapper, rand changes to dataset imgs, induce variability to dataset
-def custom_mapper(dataset_dicts):
-    dataset_dicts = copy.deepcopy(dataset_dicts)  # it will be modified by code below
-    image = utils.read_image(dataset_dicts["file_name"], format="BGR")
+def custom_mapper(dataset_dict):
+    image = utils.read_image(dataset_dict["file_name"], format="BGR")
     transform_list = [
         T.Resize((800,800)),
         T.RandomBrightness(0.8, 1.8),
@@ -195,19 +115,19 @@ def custom_mapper(dataset_dicts):
         T.RandomLighting(0.7),
         T.RandomFlip(prob=0.4, horizontal=False, vertical=True),
     ]
-  
+
     image, transforms = T.apply_transform_gens(transform_list, image)
-    dataset_dicts["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
+    dataset_dict["image"] = torch.as_tensor(image.transpose(2, 0, 1).astype("float32"))
 
     annos = [
         utils.transform_instance_annotations(obj, transforms, image.shape[:2])
-        for obj in dataset_dicts.pop("annotations")
+        for obj in dataset_dict.pop("annotations")
         if obj.get("iscrowd", 0) == 0
     ]
   
     instances = utils.annotations_to_instances(annos, image.shape[:2])
-    dataset_dicts["instances"] = utils.filter_empty_instances(instances)
-    return dataset_dicts
+    dataset_dict["instances"] = utils.filter_empty_instances(instances)
+    return dataset_dict
 
 ## Replace default trainer, subs 2x 1D method
 class CustomTrainer(DefaultTrainer):
@@ -215,65 +135,80 @@ class CustomTrainer(DefaultTrainer):
     def build_train_loader(cls, cfg):
         return build_detection_train_loader(cfg, mapper=custom_mapper)
 
-## Load custom dataset
-# Load detection classes  thing_classes=["Scale bar","Wall thickness of polyHIPEs","Pore throats of polyHIPEs","Pores of polyHIPEs"])
-
-# csv_file_path = '/home/deamoon_uw_nn/classes.csv'
-
-# # Lists to store class names and colors
-# det_classes = []
-# det_colors = []
-
-# with open(csv_file_path, newline='') as f:
-#     reader = csv.DictReader(f)
-#     for row in reader:
-#         det_classes.append(row['className'])
-#         # Assuming red, green, blue are stored as separate columns
-#         red = int(row['red'])
-#         green = int(row['green'])
-#         blue = int(row['blue'])
-#         det_colors.append((red, green, blue))
-
-
-## Load custom dataset, !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! CHANGE THING CLASSES TO LOAD FROM FILE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#Dataset load
-
 # keywords = ["Train", "Test"]
 # for d in keywords:
 #     #DatasetCatalog.register("multiclass_" + d, lambda d=d: get_superannotate_dicts("dataset/multiclass/" + d, "dataset/multiclass/train/*.json"))
-#     DatasetCatalog.register("multiclass_" + d, lambda d=d: get_fiftyone_dicts("/home/deamoon_uw_nn/DATASET/" + d + "/"))
-#     MetadataCatalog.get("multiclass_Train").set( thing_classes=["scale","wall","throat","pore"])
+#     DatasetCatalog.register("multiclass_" + d, lambda d=d: get_superannotate_dicts("/home/deamoon_uw_nn/DATASET/" + d + "/", 
+#                                                                                    "/home/deamoon_uw_nn/DATASET/" + d + "/"))
+#     MetadataCatalog.get("multiclass_Train").set( thing_classes=["throat","pore"])
   
-# multiclass_metadata = MetadataCatalog.get("multiclass_Train").set( thing_classes=["scale","wall","throat","pore"])
-# multiclass_test_metadata = MetadataCatalog.get("multiclass_Test").set( thing_classes=["scale","wall","throat","pore"])
+# multiclass_metadata = MetadataCatalog.get("multiclass_Train").set( thing_classes=["throat","pore"])
+# multiclass_test_metadata = MetadataCatalog.get("multiclass_Test").set( thing_classes=["throat","pore"])
 
-keywords = ["Train", "Test"]
-for d in keywords:
-    #DatasetCatalog.register("multiclass_" + d, lambda d=d: get_superannotate_dicts("dataset/multiclass/" + d, "dataset/multiclass/train/*.json"))
-    DatasetCatalog.register("multiclass_" + d, lambda d=d: get_superannotate_dicts("/home/deamoon_uw_nn/DATASET/" + d + "/", 
-                                                                                   "/home/deamoon_uw_nn/DATASET/" + d + "/"))
-    MetadataCatalog.get("multiclass_Train").set( thing_classes=["throat","pore"])
-  
-multiclass_metadata = MetadataCatalog.get("multiclass_Train").set( thing_classes=["throat","pore"])
-multiclass_test_metadata = MetadataCatalog.get("multiclass_Test").set( thing_classes=["throat","pore"])
+def register_datasets(dataset_paths):
+    for dataset_name, paths in dataset_paths.items():
+        img_dir, label_dir = paths
+        DatasetCatalog.register(dataset_name, lambda d=dataset_name: get_superannotate_dicts(img_dir, label_dir))
+        MetadataCatalog.get(dataset_name).set(thing_classes=["throat", "pore"])
 
-## Def det2 hyperparameters !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DO OPTUNA OPTIMIZATION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-cfg = get_cfg()
-cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
-cfg.DATASETS.TRAIN = ("multiclass_Train",)
-cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
-cfg.DATALOADER.NUM_WORKERS = 2
-cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")
-cfg.SOLVER.IMS_PER_BATCH = 2
-cfg.SOLVER.BASE_LR = 0.00025
-cfg.SOLVER.MAX_ITER = 1000
-cfg.SOLVER.STEPS = []
-cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4
-cfg.MODEL.DEVICE = "cuda"
+# Example usage:
+dataset_paths = {
+    "dataset1": ("/path/to/dataset1/images", "/path/to/dataset1/labels"),
+    "dataset2": ("/path/to/dataset2/images", "/path/to/dataset2/labels"),
+    "dataset3": ("/path/to/dataset3/images", "/path/to/dataset3/labels"),
+}
 
-## Train model
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = CustomTrainer(cfg)
-trainer.resume_or_load(resume=False)
-trainer.train()
+register_datasets(dataset_paths)
+
+# ## Def det2 hyperparameters !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DO OPTUNA OPTIMIZATION !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# cfg = get_cfg()
+# cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+# cfg.DATASETS.TRAIN = ("multiclass_Train",)
+# cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
+# cfg.DATALOADER.NUM_WORKERS = 2
+# cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")
+# cfg.SOLVER.IMS_PER_BATCH = 2
+# cfg.SOLVER.BASE_LR = 0.00025
+# cfg.SOLVER.MAX_ITER = 1000
+# cfg.SOLVER.STEPS = []
+# cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32
+# cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4
+# cfg.MODEL.DEVICE = "cuda"
+
+# ## Train model
+# os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+# trainer = CustomTrainer(cfg)
+# trainer.resume_or_load(resume=False)
+# trainer.train()
+
+def train_and_save_models(datasets, output_dir):
+    for dataset_name in datasets:
+        cfg = get_cfg()
+        cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml"))
+        cfg.DATASETS.TRAIN = (dataset_name,)
+        cfg.DATASETS.TEST = ()
+        cfg.DATALOADER.NUM_WORKERS = 2
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")
+        cfg.SOLVER.IMS_PER_BATCH = 8
+        cfg.SOLVER.BASE_LR = 0.00025
+        cfg.SOLVER.MAX_ITER = 1000
+        cfg.SOLVER.STEPS = []
+        cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 32
+        cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2  # Assuming 2 classes (throat, pore)
+        cfg.MODEL.DEVICE = "cuda"
+        
+        dataset_output_dir = os.path.join(output_dir, dataset_name)
+        os.makedirs(dataset_output_dir, exist_ok=True)
+        cfg.OUTPUT_DIR = dataset_output_dir
+        
+        trainer = CustomTrainer(cfg)
+        trainer.resume_or_load(resume=False)
+        trainer.train()
+        
+        # Save the model
+        model_path = os.path.join(dataset_output_dir, "model_final.pth")
+        torch.save(trainer.model.state_dict(), model_path)
+        print(f"Model trained on {dataset_name} saved to {model_path}")
+
+# Example usage:
+train_and_save_models(dataset_paths.keys(), "./trained_models")
