a382a5b49f0ffc2925b1e25164410676cb7250a3
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-06-11 12:51:46 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -534,7 +534,7 @@ def midpoint(ptA, ptB):
     """
     return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
 
-def GetInference():
+def GetInference(dataset_name):
     """
     Performs inference on an image and saves the predicted instances.
 
@@ -543,9 +543,9 @@ def GetInference():
     # dataset_info = read_dataset_info('./uw-com-vision/dataset_info.json')
     # register_datasets(dataset_info)
     
-    # trained_model_paths = get_trained_model_paths("./trained_models")
-    # selected_model_dataset = dataset_name  # User-selected model
-    # predictor = choose_and_use_model(trained_model_paths, selected_model_dataset)
+    trained_model_paths = get_trained_model_paths("./trained_models")
+    selected_model_dataset = dataset_name  # User-selected model
+    predictor = choose_and_use_model(trained_model_paths, selected_model_dataset)
     outputs = predictor(im)
 
     # Get all instances
@@ -666,7 +666,7 @@ def run_inference(dataset_name, output_dir, visualize=False):
                     scale_len = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
                     um_pix = float(psum) / scale_len
     
-                GetInference()  # Ensure this function is correctly defined elsewhere
+                GetInference(dataset_name)  # Ensure this function is correctly defined elsewhere
                 GetCounts()  # Ensure this function is correctly defined elsewhere
     
                 outputs = predictor(im)
