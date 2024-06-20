9c7d4aeb838a9f145cb4d8dea410d3d23213edd4
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-06-20 09:33:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,10 @@ def main():
         '--upload', action='store_true', default=True,
         help="Flag to upload results to Google Cloud Storage after executing the task. Default is True."
     )
+    parser.add_argument(
+    '--threshold', type=float, default=0.65,
+    help="Threshold for model ROI heads score."
+    )
     
     args = parser.parse_args()
 
@@ -98,7 +102,8 @@ def main():
         os.system("rm -f *.png")
         os.system("rm -f *.csv")
         os.system("rm -f *.jpg")
-        run_inference(args.dataset_name, output_dir, args.visualize)
+        # run_inference(args.dataset_name, output_dir, args.visualize)
+        run_inference(args.dataset_name, output_dir, args.visualize, args.threshold)
 
     if args.upload:
         print(f"Uploading results for dataset {args.dataset_name} to bucket...")
