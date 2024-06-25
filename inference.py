6d496a792c3b552d6cfcea18cd2eaf1fb59c6814
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-06-25 12:45:53 +0200

    Update inference.py

--- inference.py
+++ inference.py
@@ -536,47 +536,47 @@ def read_dataset_info(file_path):
         dataset_info = {k: tuple(v) if isinstance(v, list) else v for k, v in data.items()}
     return dataset_info
 
-# def rgb_to_hsv(r, g, b):
-#     MAX_PIXEL_VALUE = 255.0
+def rgb_to_hsv(r, g, b):
+    MAX_PIXEL_VALUE = 255.0
 
-#     r = r / MAX_PIXEL_VALUE
-#     g = g / MAX_PIXEL_VALUE
-#     b = b / MAX_PIXEL_VALUE
+    r = r / MAX_PIXEL_VALUE
+    g = g / MAX_PIXEL_VALUE
+    b = b / MAX_PIXEL_VALUE
 
-#     max_val = max(r, g, b)
-#     min_val = min(r, g, b)
-#     v = max_val
+    max_val = max(r, g, b)
+    min_val = min(r, g, b)
+    v = max_val
 
-#     if max_val == 0.0:
-#         s = 0
-#         h = 0
-#     elif (max_val - min_val) == 0.0:
-#         s = 0
-#         h = 0
-#     else:
-#         s = (max_val - min_val) / max_val
+    if max_val == 0.0:
+        s = 0
+        h = 0
+    elif (max_val - min_val) == 0.0:
+        s = 0
+        h = 0
+    else:
+        s = (max_val - min_val) / max_val
 
-#         if max_val == r:
-#             h = 60 * ((g - b) / (max_val - min_val)) + 0
-#         elif max_val == g:
-#             h = 60 * ((b - r) / (max_val - min_val)) + 120
-#         else:
-#             h = 60 * ((r - g) / (max_val - min_val)) + 240
+        if max_val == r:
+            h = 60 * ((g - b) / (max_val - min_val)) + 0
+        elif max_val == g:
+            h = 60 * ((b - r) / (max_val - min_val)) + 120
+        else:
+            h = 60 * ((r - g) / (max_val - min_val)) + 240
 
-#     if h < 0:
-#         h = h + 360.0
+    if h < 0:
+        h = h + 360.0
 
-#     h = h / 2
-#     s = s * MAX_PIXEL_VALUE
-#     v = v * MAX_PIXEL_VALUE
+    h = h / 2
+    s = s * MAX_PIXEL_VALUE
+    v = v * MAX_PIXEL_VALUE
 
-#     return h, s, v
+    return h, s, v
 
-def rgb_to_wavelength(b, g, r):
-    hsv_image = cv2.cvtColor(np.uint8([[[b, g, r]]]), cv2.COLOR_BGR2HSV)[0][0]
-    h, s, v = hsv_image
-    wavelength = hue_to_wavelength(h)
-    return wavelength
+# def rgb_to_wavelength(b, g, r):
+#     hsv_image = cv2.cvtColor(np.uint8([[[b, g, r]]]), cv2.COLOR_BGR2HSV)[0][0]
+#     h, s, v = hsv_image
+#     wavelength = hue_to_wavelength(h)
+#     return wavelength
 
 def hue_to_wavelength(hue):
     # There is nothing corresponding to magenta in the light spectrum,
@@ -589,10 +589,10 @@ def hue_to_wavelength(hue):
     wavelength = 620 - 170 / 270 * hue
     return wavelength
 
-# def rgb_to_wavelength(r, g, b):
-#     h, s, v = rgb_to_hsv(r, g, b)
-#     wavelength = hue_to_wavelength(h)
-#     return wavelength
+def rgb_to_wavelength(r, g, b):
+    h, s, v = rgb_to_hsv(r, g, b)
+    wavelength = hue_to_wavelength(h)
+    return wavelength
 
 def detect_arrows(image):
     """
