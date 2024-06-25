99b1a7977ef1ea6b666a223136745f7f41e31d38
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-06-25 10:59:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -374,12 +374,12 @@ if st.session_state.show_images:
                 if dataset_name != 'hw_patterns':
                     download_name = 'results_pores.csv'
                 else:
-                    download_name = 'results_flows.csv'
+                    download_name = 'results_cyclones.csv'
             elif csv_name == 'results_x_pred_0.csv':
                 if dataset_name != 'hw_patterns':
                     download_name = 'results_throats.csv'
                 else:
-                    download_name = 'results_cyclones.csv'
+                    download_name = 'results_flows.csv'
             else:
                 continue  # Skip files that don't match the specific names
             st.download_button(
