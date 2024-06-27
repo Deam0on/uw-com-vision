4f8817d50f51d69bfb925773ff9ca724ab0a6ca7
Deam0on <71432962+Deam0on@users.noreply.github.com>
2024-06-27 11:20:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,6 @@ import time
 import zipfile
 from tempfile import TemporaryDirectory
 
-
 # Add these lines at the beginning of the script
 ADMIN_PASSWORD = "deamoon_uw_nn"
 
@@ -26,6 +25,17 @@ GCS_ARCHIVE_FOLDER = 'Archive'
 GCS_DATASET_INFO_PATH = 'dataset_info.json'
 
 def create_zip_from_gcs(bucket_name, folder, zip_name="archive.zip"):
+    """
+    Creates a ZIP archive from files in a GCS bucket folder.
+
+    Parameters:
+    - bucket_name: Name of the GCS bucket.
+    - folder: Folder in the GCS bucket.
+    - zip_name: Name of the ZIP archive to create.
+
+    Returns:
+    - bytes: ZIP archive as bytes.
+    """
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     blobs = bucket.list_blobs(prefix=folder)
@@ -42,8 +52,13 @@ def create_zip_from_gcs(bucket_name, folder, zip_name="archive.zip"):
             bytes_zip = f.read()
     return bytes_zip
 
-
 def check_password():
+    """
+    Checks the entered password against the admin password.
+
+    Returns:
+    - bool: True if the password is correct, else False.
+    """
     def password_entered():
         if st.session_state["password"] == ADMIN_PASSWORD:
             st.session_state["password_correct"] = True
@@ -65,6 +80,16 @@ def _item_to_value(iterator, item):
     return item
 
 def list_directories(bucket_name, prefix):
+    """
+    Lists directories in a GCS bucket with the given prefix.
+
+    Parameters:
+    - bucket_name: Name of the GCS bucket.
+    - prefix: Prefix to filter directories.
+
+    Returns:
+    - list: List of directory paths.
+    """
     if prefix and not prefix.endswith('/'):
         prefix += '/'
 
@@ -89,8 +114,16 @@ def list_directories(bucket_name, prefix):
 
     return [x for x in iterator]
 
-# Function to run shell commands
 def run_command(command):
+    """
+    Runs a shell command.
+
+    Parameters:
+    - command: Command to run.
+
+    Returns:
+    - tuple: (stdout, stderr, success_flag)
+    """
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     stdout, stderr = [], []
     while process.poll() is None:
@@ -102,15 +135,33 @@ def run_command(command):
     process.stderr.close()
     return '\n'.join(stdout), stderr_output, process.returncode == 0
 
-# Function to list .png files in a GCS folder
 def list_png_files_in_gcs_folder(bucket_name, folder):
+    """
+    Lists .png files in a GCS folder.
+
+    Parameters:
+    - bucket_name: Name of the GCS bucket.
+    - folder: Folder in the GCS bucket.
+
+    Returns:
+    - list: List of blob objects for .png files.
+    """
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     blobs = bucket.list_blobs(prefix=folder)
     return [blob for blob in blobs if blob.name.endswith('.png')]
 
-# Function to list specific .csv files in a GCS folder
 def list_specific_csv_files_in_gcs_folder(bucket_name, folder):
+    """
+    Lists specific .csv files in a GCS folder.
+
+    Parameters:
+    - bucket_name: Name of the GCS bucket.
+    - folder: Folder in the GCS bucket.
+
+    Returns:
+    - list: List of blob objects for specific .csv files.
+    """
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     blobs = bucket.list_blobs(prefix=folder)
@@ -119,42 +170,65 @@ def list_specific_csv_files_in_gcs_folder(bucket_name, folder):
         if blob.name.endswith('results_x_pred_1.csv') or blob.name.endswith('results_x_pred_0.csv')
     ]
 
-# Function to check if stderr contains errors
 def contains_errors(stderr):
+    """
+    Checks if stderr contains any errors.
+
+    Parameters:
+    - stderr: Stderr output as string.
+
+    Returns:
+    - bool: True if errors found, else False.
+    """
     error_keywords = ['error', 'failed', 'exception', 'traceback', 'critical']
     return any(keyword in stderr.lower() for keyword in error_keywords)
 
-# Function to load dataset names from JSON in GCS
 def load_dataset_names_from_gcs():
+    """
+    Loads dataset names from a JSON file in GCS.
+
+    Returns:
+    - dict: Dataset names and details.
+    """
     client = storage.Client()
     bucket = client.bucket(GCS_BUCKET_NAME)
     blob = bucket.blob(GCS_DATASET_INFO_PATH)
     
-    # Try to download the file
     try:
         data = json.loads(blob.download_as_bytes())
     except Exception as e:
-        # If the file does not exist, initialize with an empty dictionary
         st.warning(f"dataset_info.json not found. Initializing a new one.")
         data = {}
         save_dataset_names_to_gcs(data)
     
     return data
 
-# Function to save dataset names to JSON in GCS
 def save_dataset_names_to_gcs(data):
+    """
+    Saves dataset names to a JSON file in GCS.
+
+    Parameters:
+    - data: Dataset names and details.
+    """
     client = storage.Client()
     bucket = client.bucket(GCS_BUCKET_NAME)
     blob = bucket.blob(GCS_DATASET_INFO_PATH)
     blob.upload_from_string(json.dumps(data, indent=2), content_type='application/json')
     st.write("Dataset info updated in GCS.")
 
-# Function to upload files to GCS
 def upload_files_to_gcs(bucket_name, target_folder, files, overwrite):
+    """
+    Uploads files to a GCS bucket.
+
+    Parameters:
+    - bucket_name: Name of the GCS bucket.
+    - target_folder: Folder in the GCS bucket to upload files to.
+    - files: List of files to upload.
+    - overwrite: Flag to indicate whether to overwrite existing files.
+    """
     client = storage.Client()
     bucket = client.bucket(bucket_name)
     
-    # If overwriting, delete all existing blobs in the target folder
     if overwrite:
         blobs = bucket.list_blobs(prefix=target_folder)
         for blob in blobs:
@@ -179,23 +253,28 @@ def format_and_sort_folders(folders):
     """
     formatted_folders = []
     for folder in folders:
-        # Strip trailing slashes and extract the timestamp part
         folder = folder.rstrip('/')
         try:
-            # Parse and format the timestamp
             timestamp = datetime.strptime(folder.split('/')[-1], '%Y%m%d_%H%M%S')
             formatted_name = timestamp.strftime('%B %d, %Y %H:%M:%S')
             formatted_folders.append((folder, formatted_name))
         except ValueError:
-            # If parsing fails, keep the original folder name
             formatted_folders.append((folder, folder))
 
-    # Sort by the timestamp (newest first)
     formatted_folders.sort(key=lambda x: x[1], reverse=True)
     return formatted_folders
 
-# Define a function to read and calculate ETA
 def estimate_eta(task, num_images=0):
+    """
+    Estimate ETA for a task based on previous timings.
+
+    Parameters:
+    - task: Task name (e.g., 'inference', 'prepare').
+    - num_images: Number of images to process (relevant for 'inference' task).
+
+    Returns:
+    - tuple: (download_eta, task_eta, upload_eta)
+    """
     data = read_eta_data()
     if task == 'inference':
         avg_time_per_image = data.get(task, {}).get('average_time_per_image', 1)
@@ -209,8 +288,13 @@ def estimate_eta(task, num_images=0):
     
     return download_eta, task_eta, upload_eta
 
-# Define a function to read ETA data
 def read_eta_data():
+    """
+    Read ETA data from a JSON file.
+
+    Returns:
+    - dict: ETA data.
+    """
     ETA_FILE = '/home/deamoon_uw_nn/uw-com-vision/eta_data.json'
     if os.path.exists(ETA_FILE):
         with open(ETA_FILE, 'r') as file:
@@ -266,8 +350,6 @@ if check_password():
         else:
             st.warning("Please check the confirmation box to delete the dataset.")
                 
-# task = st.selectbox("Select Task", ["prepare", "train", "evaluate", "inference"])
-
 # Define the task mapping
 task_mapping = {
     "inference": "MEASUREMENT",
@@ -358,13 +440,12 @@ if st.button("Run Task"):
         st.success(f"{task.capitalize()} task completed successfully!")
 
 # Conditionally show the upload section
-# Mapping for upload folder display names
 upload_folder_mapping = {
     f"{GCS_DATASET_FOLDER}/{dataset_name}": "TRAINING DATA",
     GCS_INFERENCE_FOLDER: "MEASUREMENT DATA"
 }
 reverse_upload_folder_mapping = {v: k for k, v in upload_folder_mapping.items()}
-# Conditionally show the upload section
+
 if use_new_data:
     st.header("Upload Files to GCS")
     upload_folder_display_names = [upload_folder_mapping[f"{GCS_DATASET_FOLDER}/{dataset_name}"], upload_folder_mapping[GCS_INFERENCE_FOLDER]]
@@ -458,45 +539,6 @@ if st.session_state.show_images:
     else:
         st.write("No specific CSV files found in the selected folder.")
 
-# After displaying the inference images and CSV download buttons, add the following:
-if st.session_state.show_images:
-    st.write(f"Displaying images from folder: {folder_dropdown[1]}")  # Use formatted name for display
-    image_files = list_png_files_in_gcs_folder(GCS_BUCKET_NAME, selected_folder)  # Use original name for operations
-    if image_files:
-        for blob in image_files:
-            img_bytes = blob.download_as_bytes()
-            img = Image.open(BytesIO(img_bytes))
-            st.image(img, caption=os.path.basename(blob.name))
-    else:
-        st.write("No images found in the selected folder.")
-
-    # Button to download specific CSV files
-    csv_files = list_specific_csv_files_in_gcs_folder(GCS_BUCKET_NAME, selected_folder)  # Use original name for operations
-    if csv_files:
-        for blob in csv_files:
-            csv_bytes = blob.download_as_bytes()
-            csv_name = os.path.basename(blob.name)
-            if csv_name == 'results_x_pred_1.csv':
-                if dataset_name != 'hw_patterns':
-                    download_name = 'results_pores.csv'
-                else:
-                    download_name = 'results_cyclones.csv'
-            elif csv_name == 'results_x_pred_0.csv':
-                if dataset_name != 'hw_patterns':
-                    download_name = 'results_throats.csv'
-                else:
-                    download_name = 'results_flows.csv'
-            else:
-                continue  # Skip files that don't match the specific names
-            st.download_button(
-                label=f"Download {download_name}",
-                data=csv_bytes,
-                file_name=download_name,
-                mime='text/csv'
-            )
-    else:
-        st.write("No specific CSV files found in the selected folder.")
-
     # Add "Download All" button
     if st.button("Download All as Zip"):
         zip_bytes = create_zip_from_gcs(GCS_BUCKET_NAME, selected_folder)
@@ -506,8 +548,6 @@ if st.session_state.show_images:
             file_name="inference_results.zip",
             mime="application/zip"
         )
-
-
 
 # Help and Tips Section
 st.sidebar.title("Help & Tips")
